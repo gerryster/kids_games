067169a256ad43c7a0285275344eca8a95d06e48
Ryan Gerry <ryan.gerry@aonhewitt.com>
2013-02-10 16:17:13 -0600

    Newly minted Rails 3.2 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KidsGames::Application.config.secret_token = '333374fa4be155476ffe55187d4b11090b6cd2d90a5fec74181044ea56b1872930be0a9cd5c422c5622d4d393c0dff5348c72af601fa3a7af969deac8810ff4a'
