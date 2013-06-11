15ccad4b7e6ac2e4bfda7bcfe75649134f822c48
Ryan Gerry <gerryster@gmail.com>
2013-06-11 07:09:38 -0500

    My first Polymer element and cleaning up the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 KidsGames::Application.routes.draw do
   resources :stories
-  resources :hangman
+  match 'hangman' => 'hangman#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
